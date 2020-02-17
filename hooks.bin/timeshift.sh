cdd49c354c5ce0b19b78210cdd80fbb99ffa3d58
MohamedElsiddig <mxlr7siddig@gmail.com>
2020-02-17 22:48:19 +0200

    overdue.sh

---
+++ hooks.bin/timeshift.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#/etc/pacman.d/hooks.bin/timeshift.sh
+find "/mnt/Local Disk/timeshift/snapshots-ondemand" -mmin -60 | grep $(date +%Y-%m-%d)
+if [ $? -eq 0 ]; then
+    echo "timeshift backup canceled, time threshold not met"
+else
+    sudo -Hiu mohamedelsiddig /bin/sh -c 'DISPLAY=:0 notify-send "Timeshift_Backup_Beginning"'
+        echo " "
+            echo "Initiating Pre-Update Timeshift Snapshot"
+            echo " "
+        echo "You have 10 seconds to cancel timeshift snapshot"
+    /usr/bin/timeshift --create --comments "timeshift-pacman-hook-snapshot"
+fi
+exit
